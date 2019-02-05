3f9d7552ef7a8b04ef82a98f859fb8b6535035d2
Moritz Schwörer <mr.mosch@gmail.com>
2019-02-05 15:57:21 +0100

    add story again

--- stories/FormStory.jsx
+++ stories/FormStory.jsx
@@ -14,6 +14,7 @@ import {
   SimpleLayout,
   SquareIconButton,
   Text,
+  CreditCardInput,
   TextInput,
   TitleBar,
   ThemeProvider,
@@ -53,6 +54,7 @@ export default class FormStory extends React.Component {
                       placeholder="What's your last name?"
                       required
                     />
+                    <CreditCardInput />
                     <TextInput
                       icon="email"
                       name="email"
