1384540d58e67828e2efdffbc4aac5c395bab00a
Daniel <dan-lee@users.noreply.github.com>
2019-02-12 09:47:14 +0100

    Refactor ThemeProvider to use React context (#628)
    * Refactor ThemeProvider to use React.createContext and get rid of react-broadcast.
    
    * Fix failing RadioButton test (seems like `shallow` doesn't work here)
    
    * Clean up RadioButton and fix controlled/uncontrolled warning.
    
    * Functional!

--- src/behaviour/Theme.jsx
+++ src/behaviour/Theme.jsx
@@ -1,34 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Subscriber } from 'react-broadcast'
-
-import { THEME_CHANNEL, defaultTheme } from './ThemeProvider'
+import { ThemeConsumer } from './ThemeProvider'
 
 const createColorizor = theme => color => theme[color] || color
 
-const Theme = ({ children }, context) => {
-  if (context.broadcasts && context.broadcasts[THEME_CHANNEL]) {
-    return (
-      <Subscriber channel={THEME_CHANNEL}>
-        {theme =>
-          children({
-            theme,
-            colorize: createColorizor(theme),
-          })}
-      </Subscriber>
-    )
-  } else {
-    return children({
-      defaultTheme,
-      colorize: createColorizor(defaultTheme),
-    })
-  }
-}
+const Theme = ({ children }) => (
+  <ThemeConsumer>
+    {theme =>
+      children({
+        theme,
+        colorize: createColorizor(theme),
+      })
+    }
+  </ThemeConsumer>
+)
 
 Theme.propTypes = {
   children: PropTypes.func.isRequired,
 }
-
-Theme.contextTypes = Subscriber.contextTypes
 
 export default Theme
