49174a7a021286475fef130952c5ca4bc038f174
Alysa Kikuchi <kialysa@yahoo.com>
2019-03-11 10:12:39 +0100

    add example of theme spinner and user prop color

--- stories/SpinnerStory.jsx
+++ stories/SpinnerStory.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { ResourceProvider, Spinner, ThemeProvider } from '../src/'
+import Text from '../src/atoms/Text'
 
 const SpinnerStory = () => (
-  <ThemeProvider>
+  <ThemeProvider theme={{ primary: 'pink' }}>
     <ResourceProvider>
+      <Text>This Spinner takes its color from the theme.</Text>
       <Spinner />
+      <Spinner color="#800080" />
     </ResourceProvider>
   </ThemeProvider>
 )
