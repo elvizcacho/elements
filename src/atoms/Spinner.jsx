0a59e2942831b78580527aa8a3d2188588bf59bc
Alysa Kikuchi <kialysa@yahoo.com>
2019-03-14 16:34:37 +0100

    check typeof window

--- src/atoms/Spinner.jsx
+++ src/atoms/Spinner.jsx
@@ -42,7 +42,7 @@ const styles = (color, size) => ({
  */
 
 const Spinner = ({ color = ColorPalette.blue, size = 30 }) =>
-  isIE11(window && window.navigator.userAgent) ? (
+  isIE11(typeof window !== 'undefined' && window.navigator.userAgent) ? (
     <div {...css(styles(color, size).spinner)} />
   ) : (
     <svg
