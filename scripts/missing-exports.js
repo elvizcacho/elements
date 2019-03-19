6c226823775170d2ac7bed3f7d3845020a9a927d
Daniel Lehr <daniel.lehr@allthings.me>
2019-03-19 08:25:47 +0100

    Wording of script

--- scripts/missing-exports.js
+++ scripts/missing-exports.js
@@ -30,6 +30,11 @@ const diff = (a, b) => new Set([...a].filter(x => !b.has(x)))
 const missing = Array.from(diff(all, used))
 
 if (missing.length > 0) {
-  console.log(`Missing following exports: ${missing.join(', ')}`)
-  process.exit(1)
+  console.warn(
+    'Following files are not being exported and therefore not being picked up. Did you forget to export them?'
+  )
+  missing.forEach(m => console.warn(`- ${m}`))
+  console.warn(
+    'If they are only used internally and should not be exposed, add them to the "ignoredMissingImports" field in the package.json.'
+  )
 }
