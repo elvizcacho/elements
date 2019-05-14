fc9c0b987ed786c9f26bb6ff5d81868537cc09a1
Alysa Kikuchi <kialysa@yahoo.com>
2019-05-14 11:30:22 +0200

    make padding right important

--- src/organisms/Typeahead.jsx
+++ src/organisms/Typeahead.jsx
@@ -374,7 +374,7 @@ export default class Typeahead extends React.PureComponent {
                         boxShadow: 'none',
                         color: '#999',
                         opacity: 1,
-                        paddingRight: '50px',
+                        paddingRight: '50px !important',
                         height: INPUT_FIELD_HEIGHT,
                       })}
                     />
@@ -407,7 +407,7 @@ export default class Typeahead extends React.PureComponent {
                       boxShadow: 'none',
                       color: '#000',
                       outline: 'none',
-                      paddingRight: '50px',
+                      paddingRight: '50px !important',
                       width: '100%',
                       height: INPUT_FIELD_HEIGHT,
                     })}
