0840993512af9bd5b6be94208155ae18215984e5
janrembold <janrembold@gmx.net>
2019-07-12 14:10:52 +0200

    feat: add searchable dropdown component

--- doc/reference/SearchableDropdown.md
+++ doc/reference/SearchableDropdown.md
@@ -10,11 +10,16 @@ You can change it in "src/SearchableDropdown/SearchableDropdown.tsx" and run bui
 |clearable|boolean|If true, than the field can be cleared
 |icon|"alarm", "alarm-filled", "armchair", "armchair-filled", "arrow-down", "arrow-down-filled", "arrow-left", "arrow-left-filled", "arrow-right", "arrow-right-filled", ... 147 more ...|Icon on the left of the input field
 |items **(required)**||The dropdown items to show
-|initialSelectedItem|ItemType|Initially selected item - this value is uncontrolled
+|initialSelectedItem|IDropdownItem|Initially selected item - this value is uncontrolled
 |label|string|A floating label
+|loadMoreText|string|The "Load more" text
 |menuHeight|number|The height of the menu in pixels.
 |name|string|For forms
-|onSelect|((item: ItemType) => void)|Callback triggered when clearing the selection.
+|noResultsText|string|The text is shown if no result was found
+|onLoadMore|(() => void)|Callback triggered when clicking on "Load more" in items list
+|onSelect|((item: IDropdownItem) => void)|Callback triggered when clearing the selection.
+|onSearch|((event: SyntheticEvent<Element, Event>) => void)|Callback triggered when search value changes
 |placeholder|string|The placeholder displayed in the input field.
 |placement|EDirection|If "top", then the list should be reversed and extended upwards, if "bottom" (default) then downwards
-|selectedItem|ItemType|Selected item - this item can be controlled
+|searchTerm|string|The search term
+|selectedItem|IDropdownItem|Selected item - this item can be controlled
--- src/SearchableDropdown/SearchableDropdown.tsx
+++ src/SearchableDropdown/SearchableDropdown.tsx
@@ -1,13 +1,6 @@
-import React, {
-  ReactNode,
-  createRef,
-  MouseEvent,
-  useEffect,
-  useState,
-  SyntheticEvent,
-} from 'react'
-import Downshift from 'downshift'
-import { css, keyframes } from 'glamor'
+import React, { createRef, useState, SyntheticEvent } from 'react'
+import Downshift, { StateChangeOptions, DownshiftState } from 'downshift'
+import { css } from 'glamor'
 import Relative from '../Relative/index'
 import Absolute from '../Absolute/index'
 import View from '../View/index'
@@ -18,19 +11,6 @@ import Input from '../Input/index'
 import { alpha, ColorPalette } from '@allthings/colors'
 import Icon, { IconType } from '../Icon/index'
 import { noop } from '@babel/types'
-import { TextInput } from '../index'
-
-const bounceDownwardsAnim = keyframes('bounce', {
-  '0%, 20%, 50%, 80%, 100%': { transform: 'translateY(0)' },
-  '40%': { transform: 'translateY(-10px)' },
-  '60%': { transform: 'translateY(-10px)' },
-})
-
-const bounceUpwardsAnim = keyframes('bounce', {
-  '0%, 20%, 50%, 80%, 100%': { transform: 'translateY(0)' },
-  '40%': { transform: 'translateY(+10px)' },
-  '60%': { transform: 'translateY(+10px)' },
-})
 
 const Placement = {
   top: 'top',
@@ -47,56 +27,57 @@ const styles = {
     backgroundColor: ColorPalette.white,
   }),
   disabledInput: css({
-    width: 'calc(100% - 5px)',
-    textOverflow: 'ellipsis',
-    overflow: 'hidden',
-    whiteSpace: 'nowrap',
-    cursor: 'pointer',
-    paddingRight: '30px',
-    color: 'black',
     backgroundColor: 'white',
+    color: 'black',
+    cursor: 'pointer',
+    overflow: 'hidden',
+    paddingRight: '30px',
     pointerEvents: 'none',
+    textOverflow: 'ellipsis',
+    whiteSpace: 'nowrap',
+    width: 'calc(100% - 5px)',
   }),
   iconWrapper: css({
     cursor: 'pointer',
     height: '100%',
-    width: INPUT_FIELD_HEIGHT,
     padding: '16px 16px 16px 20px',
   }),
   label: css({
     width: '100%',
-    // position: 'absolute',
-    // left: 15,
-    // fontSize: 10,
-    // opacity: 0,
-    // transition: 'all .225s ease-out',
   }),
-  list: (isOpen: boolean, menuHeight: number, placement: EDirection) =>
+  list: (menuHeight: number, placement: EDirection) =>
     css({
-      boxShadow: isOpen && '1px 1px 3px rgba(29, 29, 29, 0.125)',
+      bottom: placement === Placement.top && INPUT_FIELD_HEIGHT,
+      boxShadow: '1px 1px 3px rgba(29, 29, 29, 0.125)',
       maxHeight: menuHeight,
       overflowX: 'hidden',
       overflowY: 'auto',
-      position: 'absolute',
-      bottom: placement === Placement.top && INPUT_FIELD_HEIGHT,
       width: '100%',
       zIndex: 9999,
     }),
+  listItem: css({
+    ':hover': {
+      backgroundColor: alpha(ColorPalette.background.bright, 0.5, true),
+    },
+  }),
+  searchInput: css({
+    borderTop: `1px solid ${ColorPalette.lightGrey}`,
+  }),
   wrapper: (isOpen: boolean) =>
     css({
       alignItems: 'stretch',
+      backgroundColor: ColorPalette.white,
       border: 'none',
       boxShadow: isOpen ? '1px 1px 3px rgba(29, 29, 29, 0.125)' : '',
       display: 'flex',
       flexDirection: 'column',
       padding: 0,
       width: '100%',
-      backgroundColor: ColorPalette.white,
     }),
 }
 
-type ItemType = {
-  label: ReactNode
+export interface IDropdownItem {
+  label: string
   value: string
 }
 
@@ -111,23 +92,33 @@ interface ISearchableDropdownProps {
   /** Icon on the left of the input field */
   icon?: IconType
   /** The dropdown items to show */
-  items: ItemType[]
+  items: IDropdownItem[]
   /** Initially selected item - this value is uncontrolled */
-  initialSelectedItem?: ItemType
+  initialSelectedItem?: IDropdownItem
   /** A floating label */
   label?: string
+  /** The "Load more" text */
+  loadMoreText?: string
   /** The height of the menu in pixels. */
   menuHeight?: number
   /** For forms */
   name?: string
+  /** The text is shown if no result was found */
+  noResultsText?: string
+  /** Callback triggered when clicking on "Load more" in items list */
+  onLoadMore?: () => void
   /** Callback triggered when clearing the selection. */
-  onSelect?: (item: ItemType) => void
+  onSelect?: (item: IDropdownItem) => void
+  /** Callback triggered when search value changes */
+  onSearch?: (event: SyntheticEvent) => void
   /** The placeholder displayed in the input field. */
   placeholder?: string
   /** If "top", then the list should be reversed and extended upwards, if "bottom" (default) then downwards */
   placement?: EDirection
+  /** The search term */
+  searchTerm?: string
   /** Selected item - this item can be controlled */
-  selectedItem?: ItemType
+  selectedItem?: IDropdownItem
 }
 
 const SearchableDropdown = ({
@@ -136,17 +127,21 @@ const SearchableDropdown = ({
   items,
   initialSelectedItem,
   label = '',
+  loadMoreText = '',
   menuHeight = INPUT_FIELD_HEIGHT,
   name = '',
+  noResultsText = '',
+  onLoadMore = noop,
+  onSearch = noop,
   onSelect = noop,
   placeholder = '',
   placement = EDirection.BOTTOM,
+  searchTerm = '',
   selectedItem,
 }: ISearchableDropdownProps) => {
   const [showScrollArrow, setShowScrollArrow] = useState(false)
 
-  const listRef = createRef<HTMLDivElement>()
-  const searchRef = createRef<HTMLDivElement>()
+  const searchRef: any = createRef()
 
   const handleListScroll = (e: SyntheticEvent) => {
     if (showScrollArrow && (e.target as any).scrollTop > 0) {
@@ -154,78 +149,90 @@ const SearchableDropdown = ({
     }
   }
 
-  const showArrowIfNecessary = () =>
-    listRef.current &&
-    menuHeight &&
-    setShowScrollArrow(listRef.current.scrollHeight > menuHeight)
-
-  const handleIconClick = (
-    event: MouseEvent<HTMLElement>,
-    showClearIcon: boolean,
+  const handleClearIconClick = (
+    event: SyntheticEvent,
     clearSelection: () => void,
   ) => {
-    console.log('TODO: handle icon click', event, showClearIcon, clearSelection)
-    // showClearIcon
-    //   ? (e: MouseEvent<HTMLElement>) => {
-    //       e.stopPropagation()
-    //       clearSelection()
-    //     }
-    //   : undefined
+    event.stopPropagation()
+    clearSelection()
   }
 
-  const renderScrollArrow = () => {
-    if (!showScrollArrow) {
-      return
+  const stateReducer = (
+    _: DownshiftState<any>,
+    changes: StateChangeOptions<any>,
+  ) => {
+    if (changes && changes.isOpen) {
+      setTimeout(
+        () =>
+          searchRef.current && (searchRef.current as HTMLInputElement).focus(),
+        0,
+      )
     }
 
-    return (
-      <Absolute
-        bottom={placement === Placement.bottom ? 15 : undefined}
-        top={placement === Placement.top ? 15 : undefined}
-        right={15}
-      >
-        {placement === Placement.top ? (
-          <Icon
-            color="black"
-            name="arrow-up"
-            size="xs"
-            {...css({
-              animation: `${bounceUpwardsAnim} 2500ms 2`,
-            })}
-          />
-        ) : (
-          <Icon
-            color="black"
-            name="arrow-down"
-            size="xs"
-            {...css({
-              animation: `${bounceDownwardsAnim} 2500ms 2`,
-            })}
-          />
-        )}
-      </Absolute>
-    )
+    return changes
   }
 
-  useEffect(() => {
-    showArrowIfNecessary()
-  })
+  const renderListItems = (getItemProps: (options: object) => void) => (
+    <Relative {...styles.searchInput}>
+      <List>
+        <ListItem padded={false}>
+          <Input
+            ref={searchRef}
+            hasRightIcon={false}
+            name={`search-${Date.now()}`}
+            onChange={onSearch}
+            placeholder="Search"
+            value={searchTerm}
+            type="text"
+          />
+        </ListItem>
+      </List>
+      <List
+        direction={placement === Placement.top ? 'column-reverse' : 'column'}
+        onScroll={handleListScroll}
+        {...styles.list(menuHeight, placement)}
+      >
+        {items.map((item, index) => (
+          <ListItem
+            {...getItemProps({
+              index,
+              item,
+              key: item.value,
+            })}
+            {...styles.listItem}
+          >
+            <Text size="l" {...styles.label}>
+              {item.label}
+            </Text>
+          </ListItem>
+        ))}
+        {noResultsText && items.length === 0 && (
+          <ListItem>
+            <Text size="l" {...styles.label}>
+              {noResultsText}
+            </Text>
+          </ListItem>
+        )}
+        {loadMoreText && items.length && (
+          <ListItem onClick={onLoadMore} {...styles.listItem}>
+            <Text size="l" {...styles.label}>
+              {loadMoreText}
+            </Text>
+          </ListItem>
+        )}
+      </List>
+    </Relative>
+  )
 
   return (
     <Downshift
       itemToString={item => (item ? item.label : '')}
-      onChange={onSelect}
       initialSelectedItem={initialSelectedItem}
+      onChange={onSelect}
       selectedItem={selectedItem}
+      stateReducer={stateReducer}
     >
-      {({
-        isOpen,
-        toggleMenu,
-        getItemProps,
-        highlightedIndex,
-        clearSelection,
-        selectedItem,
-      }) => {
+      {({ isOpen, toggleMenu, getItemProps, clearSelection, selectedItem }) => {
         const showClearIcon = clearable && selectedItem && selectedItem.value
 
         return (
@@ -244,70 +251,33 @@ const SearchableDropdown = ({
                   {...styles.disabledInput}
                 />
 
-                <Absolute
-                  top={0}
-                  right={0}
-                  {...styles.iconWrapper}
-                  onClick={event =>
-                    handleIconClick(event, showClearIcon, clearSelection)
-                  }
-                >
-                  {showClearIcon && (
-                    <Icon color="black" name="remove-light" size={10} />
-                  )}
+                <Absolute top={0} right={0} {...styles.iconWrapper}>
+                  <View direction="row">
+                    {showClearIcon && (
+                      <Icon
+                        color="black"
+                        name="remove-light"
+                        size={10}
+                        onClick={event =>
+                          handleClearIconClick(event, clearSelection)
+                        }
+                        {...css({ marginRight: 10, display: 'inline-block' })}
+                      />
+                    )}
 
-                  <Icon
-                    color="black"
-                    name={
-                      placement === Placement.top ? 'arrow-up' : 'arrow-down'
-                    }
-                    size={10}
-                  />
+                    <Icon
+                      color="black"
+                      name={
+                        placement === Placement.top ? 'arrow-up' : 'arrow-down'
+                      }
+                      size={10}
+                    />
+                  </View>
                 </Absolute>
               </View>
             </Relative>
 
-            {isOpen && (
-              <Relative>
-                <TextInput
-                  name={`search-${Date.now()}`}
-                  type="text"
-                  placeholder="Search"
-                  hasRightIcon={false}
-                  forwardedRef={searchRef}
-                />
-                <List
-                  direction={
-                    placement === Placement.top ? 'column-reverse' : 'column'
-                  }
-                  onScroll={handleListScroll}
-                  ref={listRef}
-                  {...styles.list(isOpen, menuHeight, placement)}
-                >
-                  {items.map((item, index) => (
-                    <ListItem
-                      key={item.value}
-                      {...getItemProps({
-                        index,
-                        item,
-                        key: item.value,
-                        style: {
-                          backgroundColor:
-                            highlightedIndex === index
-                              ? alpha(ColorPalette.background.bright, 0.5, true)
-                              : ColorPalette.background.white,
-                        },
-                      })}
-                    >
-                      <Text size="l" {...styles.label}>
-                        {item.label}
-                      </Text>
-                    </ListItem>
-                  ))}
-                  {renderScrollArrow()}
-                </List>
-              </Relative>
-            )}
+            {isOpen && renderListItems(getItemProps)}
           </div>
         )
       }}
