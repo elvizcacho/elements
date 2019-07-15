b66efa5f22af068e7f2d93a98acbe195f14372a8
janrembold <janrembold@gmx.net>
2019-07-15 16:59:04 +0200

    add tests and snaps

--- src/SearchableDropdown/SearchableDropdown.test.tsx
+++ src/SearchableDropdown/SearchableDropdown.test.tsx
@@ -1,80 +1,129 @@
 import React from 'react'
-import renderer from 'react-test-renderer'
-import { ThemeProvider, ResourceProvider, Dropdown } from '../index'
+import { render, fireEvent } from '@testing-library/react'
+import { ThemeProvider, ResourceProvider, SearchableDropdown } from '../index'
+import { EDropdownDirection } from '../enums'
 
-describe('Dropdown', () => {
+const items = [
+  {
+    label: 'dog',
+    value: 'dog',
+  },
+  {
+    label: 'cat',
+    value: 'cat',
+  },
+  {
+    label: 'apple',
+    value: 'apple',
+  },
+]
+
+describe('SearchableDropdown', () => {
   it('should render with placement bottom (default)', () => {
-    const tree = renderer
-      .create(
-        <ThemeProvider>
-          <ResourceProvider>
-            <Dropdown
-              initialSelectedItem={{
-                label: 'dog',
-                value: 'dog',
-              }}
-              label="some label"
-              menuHeight={200}
-              onSelect={() => {}}
-              placeholder="select something..."
-              clearable
-              items={[
-                {
-                  label: 'dog',
-                  value: 'dog',
-                },
-                {
-                  label: 'cat',
-                  value: 'cat',
-                },
-                {
-                  label: 'apple',
-                  value: 'apple',
-                },
-              ]}
-            />
-          </ResourceProvider>
-        </ThemeProvider>,
-      )
-      .toJSON()
-    expect(tree).toMatchSnapshot()
+    const dropdown = render(
+      <ThemeProvider>
+        <ResourceProvider>
+          <SearchableDropdown
+            initialSelectedItem={items[0]}
+            items={items}
+            label="some label"
+            menuHeight={200}
+            name="searchable-dropdown-form-name"
+            onSelect={() => {}}
+            placeholder="select something..."
+          />
+        </ResourceProvider>
+      </ThemeProvider>,
+    )
+
+    fireEvent.click(dropdown.getByTestId('searchable-dropdown'))
+    expect(dropdown.container).toMatchSnapshot()
   })
 
   it('should render with placement top', () => {
-    const tree = renderer
-      .create(
-        <ThemeProvider>
-          <ResourceProvider>
-            <Dropdown
-              initialSelectedItem={{
-                label: 'dog',
-                value: 'dog',
-              }}
-              label="some label"
-              menuHeight={200}
-              onSelect={() => {}}
-              placeholder="select something..."
-              clearable
-              items={[
-                {
-                  label: 'dog',
-                  value: 'dog',
-                },
-                {
-                  label: 'cat',
-                  value: 'cat',
-                },
-                {
-                  label: 'apple',
-                  value: 'apple',
-                },
-              ]}
-              placement="top"
-            />
-          </ResourceProvider>
-        </ThemeProvider>,
-      )
-      .toJSON()
-    expect(tree).toMatchSnapshot()
+    const dropdown = render(
+      <ThemeProvider>
+        <ResourceProvider>
+          <SearchableDropdown
+            initialSelectedItem={items[0]}
+            items={items}
+            label="some label"
+            menuHeight={200}
+            onSelect={() => {}}
+            placeholder="select something..."
+            placement={EDropdownDirection.TOP}
+          />
+        </ResourceProvider>
+      </ThemeProvider>,
+    )
+
+    fireEvent.click(dropdown.getByTestId('searchable-dropdown'))
+    expect(dropdown.container).toMatchSnapshot()
+  })
+
+  it('should render with icon', () => {
+    const dropdown = render(
+      <ThemeProvider>
+        <ResourceProvider>
+          <SearchableDropdown
+            icon="user"
+            initialSelectedItem={items[0]}
+            items={items}
+            label="some label"
+            menuHeight={200}
+            onSelect={() => {}}
+            placeholder="select something..."
+          />
+        </ResourceProvider>
+      </ThemeProvider>,
+    )
+
+    fireEvent.click(dropdown.getByTestId('searchable-dropdown'))
+    expect(dropdown.container).toMatchSnapshot()
+  })
+
+  it('should render with no results', () => {
+    const dropdown = render(
+      <ThemeProvider>
+        <ResourceProvider>
+          <SearchableDropdown
+            items={[]}
+            label="some label"
+            menuHeight={200}
+            noResultsText="No results found"
+            onSelect={() => {}}
+            placeholder="select something..."
+          />
+        </ResourceProvider>
+      </ThemeProvider>,
+    )
+
+    fireEvent.click(dropdown.getByTestId('searchable-dropdown'))
+
+    expect(dropdown.getByTestId('searchable-dropdown-no-results')).toBeTruthy()
+    expect(dropdown.container).toMatchSnapshot()
+  })
+
+  it('should render with load more', () => {
+    const dropdown = render(
+      <ThemeProvider>
+        <ResourceProvider>
+          <SearchableDropdown
+            items={items}
+            label="some label"
+            loadMoreText="Load more"
+            menuHeight={200}
+            noResultsText="No results found"
+            onSelect={() => {}}
+            placeholder="select something..."
+          />
+        </ResourceProvider>
+      </ThemeProvider>,
+    )
+
+    fireEvent.click(dropdown.getByTestId('searchable-dropdown'))
+
+    expect(dropdown.getByTestId('searchable-dropdown-load-more')).toBeTruthy()
+    expect(dropdown.container).toMatchSnapshot()
   })
 })
